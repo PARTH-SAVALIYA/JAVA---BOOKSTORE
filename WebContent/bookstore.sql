146b14770504ce1c78f8a4cba9f6ec914e1a7456
ankita <ankita@ankita-PC>
2019-06-04 20:56:31 +0530

    Initial commit

--- WebContent/bookstore.sql
+++ WebContent/bookstore.sql
@@ -5,8 +5,11 @@ CREATE TABLE `book` (
   `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 INSERT INTO `book` (`book_id`, `title`, `author`, `price`) VALUES
-(1, 'testsds', 'test', 1000),
-(2, 'test', 'test', 2000);
+(1, 'Game Of Thrones', 'George Raymond Richard Martin', 2825),
+(2, 'Harry Potter nove', 'J. K. Rowling', 2635),
+(3, 'Lolita', 'Vladimir Nabokov', 250),
+(4, 'In Search of Lost Time', 'Marcel Proust', 500),
+(5, 'To The Lighthouse', 'Virginia Woolf', 635);
 ALTER TABLE `book`
   ADD PRIMARY KEY (`book_id`),
   ADD UNIQUE KEY `book_id_UNIQUE` (`book_id`),
